f89295c96dc79723e5716ceb6db945e7bf950d82
Nate Sedler <nsedler1@gmail.com>
2022-01-15 19:31:02 -0500

    added env to snekdb

--- snekdb.py
+++ snekdb.py
@@ -1,9 +1,20 @@
 import mysql.connector
+import os
+
+from dotenv import load_dotenv
+
 
 class SnekDB(object):
     def __init__(self):
-        self.cnx = mysql.connector.connect(user='', password='',
-                                            host='', database='', port=3306)
+
+        db_user = os.getenv('DB_USER')
+        db_pass = os.getenv('DB_PASS')
+        db_host = os.getenv('DB_HOST')
+        db_database = os.getenv('DB_DATABASE')
+
+        load_dotenv()
+        self.cnx = mysql.connector.connect(user=db_user, password=db_pass,
+                                           host=db_host, database=db_database, port=3306)
         self.cursor = self.cnx.cursor(buffered=True)
 
     def close_db(self):
