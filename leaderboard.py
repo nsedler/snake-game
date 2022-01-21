7984e26630d64591cdcbeaad0dd13394507bc395
Nate Sedler <nsedler1@gmail.com>
2022-01-21 00:00:54 -0500

    semi-functional scoreboard

--- leaderboard.py
+++ leaderboard.py
@@ -17,7 +17,7 @@ class Leaderboard(object):
         self.manager = pygame_gui.UIManager(
             (width, height), './data/button_theme.json')
 
-        self.font = pygame.font.SysFont("Comic Sans MS", 22)
+        self.font = pygame.font.SysFont("Comic Sans MS", 18)
 
         self.start_window = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
@@ -32,18 +32,53 @@ class Leaderboard(object):
         self.leaderboard_book = self.db.get_scoreboard()
         self.leaderboard_page = 1
 
+    def get_page_count(self):
+        return len(self.leaderboard_book)
+
+    def get_current_page(self, page_number):
+        board = self.leaderboard_book[page_number - 1][page_number]
+        html_text = ''
+
+        for ScoreID, Score, Name in board:
+            html_text += f'{Score} {Name} <br>'
+
+        return html_text
+
     def draw(self):
         self.start_window.fill((0, 0, 0))
+        score_text = self.get_current_page(self.leaderboard_page)
+        score_board = pygame_gui.elements.UITextBox(
+            score_text,
+            pygame.Rect(10, 10, 300, 150),
+            manager=self.manager,
+            object_id="#scoreboard"
+        )
+
+        page_surface = self.font.render(
+            str(self.leaderboard_page), False, (248, 240, 227))
+        self.start_window.blit(page_surface, (170, 270))
 
     def game_loop(self):
 
+        page_count = self.get_page_count()
+
         while self.running:
+
             time_delta = self.clock.tick(15)/1000.0
+
             for event in pygame.event.get():
                 self.manager.process_events(event)
                 if event.type == pygame.QUIT:
                     self.db.close_db()
                     self.running = False
+                if event.type == pygame.USEREVENT:
+                    if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                        if event.ui_element == self.next_page_button:
+                            if self.leaderboard_page < page_count:
+                                self.leaderboard_page += 1
+                        elif event.ui_element == self.prev_page_button:
+                            if self.leaderboard_page > 1:
+                                self.leaderboard_page -= 1
 
             self.manager.update(time_delta)
             self.draw()
